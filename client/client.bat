1a8ef3baa96657c18bfa3eb74dd82c76424a924a
jriboux <jriboux@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-03-14 22:36:49 +0000

    added batch files so client, server and proxy can be run under windows

---
+++ client/client.bat
@@ -0,0 +1,7 @@
+echo off
+echo.set TMPCLASSPATH=%%1;%%TMPCLASSPATH%%>~tmp.bat
+set TMPCLASSPATH=
+for %%i in (lib\*.jar) do call ~tmp.bat "%CD%\%%i"
+del /F ~tmp.bat
+java -classpath %TMPCLASSPATH% org.lucane.client.Client
+set TMPCLASSPATH=
