b22348b19f67f0c30b54a78ea6fb4dd3ec8ccfda
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-03-28 18:51:17 +0000

    updated Copyright information

--- trunk/applications/reunion/src/org/lucane/applications/reunion/ReunionMessage.java
+++ trunk/applications/reunion/src/org/lucane/applications/reunion/ReunionMessage.java
@@ -1,6 +1,6 @@
 /*
  * Lucane - a collaborative platform
- * Copyright (C) 2002  Vincent Fiack <vfiack@mail15.com>
+ * Copyright (C) 2004  Jonathan Riboux <jonathan.riboux@wanadoo.fr>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -12,9 +12,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package org.lucane.applications.reunion;
 
