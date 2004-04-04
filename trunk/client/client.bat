ea43cef7c6c4c41782204086c5487c92b69efaef
jriboux <jriboux@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-04-04 08:56:04 +0000

    changed client.bat to use "start javaw -jar" instead of "java -jar" so the dos window doesn't wait for the client to terminate

--- trunk/client/client.bat
+++ trunk/client/client.bat
@@ -3,5 +3,5 @@ echo.set TMPCLASSPATH=%%1;%%TMPCLASSPATH%%>~tmp.bat
 set TMPCLASSPATH=
 for %%i in (lib\*.jar) do call ~tmp.bat "%CD%\%%i"
 del /F ~tmp.bat
-java -classpath %TMPCLASSPATH% org.lucane.client.Client
+start javaw -classpath %TMPCLASSPATH% org.lucane.client.Client
 set TMPCLASSPATH=
