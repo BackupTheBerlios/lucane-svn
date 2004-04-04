89f0aba460b9e4aa9d00b611c236b64c1f1fd71b
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-04-04 16:04:46 +0000

    updated startup scripts to use lib/ as java.library.path (to load the DLL)

--- trunk/client/client.bat
+++ trunk/client/client.bat
@@ -3,5 +3,9 @@ echo.set TMPCLASSPATH=%%1;%%TMPCLASSPATH%%>~tmp.bat
 set TMPCLASSPATH=
 for %%i in (lib\*.jar) do call ~tmp.bat "%CD%\%%i"
 del /F ~tmp.bat
-start javaw -classpath %TMPCLASSPATH% org.lucane.client.Client
+
+set VM_OPTIONS=-Djava.library.path=lib
+
+start javaw %VM_OPTIONS% -classpath %TMPCLASSPATH% org.lucane.client.Client
+
 set TMPCLASSPATH=
--- trunk/client/client.sh
+++ trunk/client/client.sh
@@ -3,5 +3,8 @@ for file in lib/*.jar
   do export CLASSPATH=$CLASSPATH:`pwd`/$file
 done
 
+# library path
+export VM_OPTIONS="-Djava.library.path=lib/"
+
 # run program
-java org.lucane.client.Client $*
+java $VM_OPTIONS org.lucane.client.Client $*
