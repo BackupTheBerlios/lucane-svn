c0e0bd2c6c2da9d854fa69cd976de778bbebd5df
jriboux <jriboux@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-05-16 12:50:43 +0000

    added a batch file to launch the client without hiding the console

---
+++ trunk/client/debug.bat
@@ -0,0 +1,11 @@
+echo off
+echo.set TMPCLASSPATH=%%1;%%TMPCLASSPATH%%>~tmp.bat
+set TMPCLASSPATH=
+for %%i in (lib\*.jar) do call ~tmp.bat "%CD%\%%i"
+del /F ~tmp.bat
+
+set VM_OPTIONS=-Djava.library.path=lib
+
+java %VM_OPTIONS% -classpath %TMPCLASSPATH% org.lucane.client.Client
+
+set TMPCLASSPATH=
