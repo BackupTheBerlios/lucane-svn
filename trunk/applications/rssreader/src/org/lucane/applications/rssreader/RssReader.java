42e03e7b91f834c5472b4511deb0a37de1ae2543
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-05-26 17:09:43 +0000

    made rssreader a little more generic

--- trunk/applications/rssreader/src/org/lucane/applications/rssreader/RssReader.java
+++ trunk/applications/rssreader/src/org/lucane/applications/rssreader/RssReader.java
@@ -1,6 +1,7 @@
 package org.lucane.applications.rssreader;
 
 import java.awt.BorderLayout;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 
@@ -9,52 +10,55 @@ import javax.swing.*;
 import org.jperdian.rss2.*;
 import org.jperdian.rss2.dom.*;
 import org.lucane.client.*;
+import org.lucane.client.widgets.DialogBox;
 import org.lucane.common.ConnectInfo;
 
-public class RssReader extends StandalonePlugin
+public class RssReader extends StandalonePlugin 
 {
-	public RssReader()
+	private static String DEFAULT_RSS =
+		"http://sourceforge.net/export/rss2_projnews.php?group_id=95945&rss_fulltext=1";
+
+	public RssReader() 
 	{
 		this.starter = true;
 	}
-	
-	public Plugin init(ConnectInfo[] friends, boolean starter) {
-		return new RssReader();
-	}  
-	
-  public void start()
-  {
-/*	System.setProperty("proxySet", "true");
-	System.setProperty("proxyHost", "cyclope");
-	System.setProperty("proxyPort", "8080");
-  */	
-	JFrame frame = new JFrame();
-	JList list = new JList();
-	DefaultListModel model = new DefaultListModel();
-	list.setCellRenderer(new Renderer());
-	list.setModel(model);
-	frame.getContentPane().add(new JScrollPane(list), BorderLayout.CENTER);
-	frame.setSize(600, 600);
-	frame.show();
-	
-	try {
-	URL sourceURL     = new URL("http://sourceforge.net/export/rss2_projnews.php?group_id=95945&rss_fulltext=1");
-	RssClient client  = new RssClient(sourceURL);
-	RssChannel channel = client.getData();
-	List items = channel.getItemList();
-	Iterator i = items.iterator();
-    while(i.hasNext())
-    	model.addElement(i.next());			
 
-	sourceURL     = new URL("http://linuxfr.org/backend.rss");
-	client  = new RssClient(sourceURL);
-	channel = client.getData();
-	items = channel.getItemList();
-	i = items.iterator();
-	while(i.hasNext())
-		model.addElement(i.next());			
-	} catch(Exception e) {
-		e.printStackTrace();
+	public Plugin init(ConnectInfo[] friends, boolean starter) 
+	{
+		return new RssReader();
 	}
-  }
+
+	public void start() 
+	{
+		String rss = getLocalConfig().get("rss.url", DEFAULT_RSS);
+
+		System.setProperty("proxySet", getLocalConfig().get("proxySet", "false"));
+		System.setProperty("proxyHost", getLocalConfig().get("proxyHost", ""));
+		System.setProperty("proxyPort", getLocalConfig().get("proxyPort", ""));
+
+		JFrame frame = new JFrame();
+		JList list = new JList();
+		DefaultListModel model = new DefaultListModel();
+		list.setCellRenderer(new Renderer());
+		list.setModel(model);
+		frame.getContentPane().add(new JScrollPane(list), BorderLayout.CENTER);
+		frame.setSize(600, 600);
+		frame.show();
+
+		try {
+			URL sourceURL = new URL(rss);
+			RssClient client = new RssClient(sourceURL);
+			RssChannel channel = client.getData();
+			frame.setTitle(channel.getTitle());
+			
+			List items = channel.getItemList();
+			Iterator i = items.iterator();
+			while (i.hasNext())
+				model.addElement(i.next());
+		}	catch (MalformedURLException mue) {
+			DialogBox.error("Wrong url : " + mue);
+		}	catch (RssException re) {
+			DialogBox.error("RSS Error : " + re);
+		}
+	}
 }
