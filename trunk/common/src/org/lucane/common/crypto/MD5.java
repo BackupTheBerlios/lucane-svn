7c8a4e7733a4014de8c431724cfc8f454aaec68a
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-06-06 09:34:48 +0000

    moved MD5 to crypto subpackage

--- trunk/common/src/org/lucane/common/crypto/MD5.java
+++ trunk/common/src/org/lucane/common/crypto/MD5.java
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.lucane.common;
+package org.lucane.common.crypto;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
