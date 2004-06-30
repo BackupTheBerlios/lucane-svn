04c98cd2ab60a74bcd99577f55d46add6b429783
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-06-30 17:13:27 +0000

    SqlService doesn't take a connection any more. it should be ok for the connection pool now.

--- trunk/applications/sqlnavigator/src/org/lucane/applications/sqlnavigator/SqlService.java
+++ trunk/applications/sqlnavigator/src/org/lucane/applications/sqlnavigator/SqlService.java
@@ -27,45 +27,39 @@ import org.lucane.server.database.*;
 
 
 public class SqlService
-  extends Service
+extends Service
 {
-	private Connection connection;
 	private DatabaseAbstractionLayer layer;
 	
-  public SqlService()
-  {
-  }
-
-  public void init(Server parent)
-  {
-  	try {
-  		this.connection = parent.getDBLayer().openConnection();
-  		this.layer = parent.getDBLayer();
-  	} catch(Exception e) {
-  		e.printStackTrace();
-  	}
-  }
-
-
-  public void process(ObjectConnection oc, Message message)
-  {
-  		SqlAction sa = (SqlAction)message.getData();
-  		switch(sa.action)
-  		{
-  			case SqlAction.GET_DRIVER_INFO:
-  				getDriverInfo(oc);
-  				break;
-  			case SqlAction.GET_TABLE_NAMES:
-  				getTableNames(oc);
-  				break;
-  			case SqlAction.EXECUTE_QUERY:
-  				executeQuery(oc, sa.params);
-  				break;
-  		}
-  }
-
-
-
+	public SqlService()
+	{
+	}
+	
+	public void init(Server parent)
+	{
+		this.layer = parent.getDBLayer();
+	}
+	
+	
+	public void process(ObjectConnection oc, Message message)
+	{
+		SqlAction sa = (SqlAction)message.getData();
+		switch(sa.action)
+		{
+			case SqlAction.GET_DRIVER_INFO:
+				getDriverInfo(oc);
+			break;
+			case SqlAction.GET_TABLE_NAMES:
+				getTableNames(oc);
+			break;
+			case SqlAction.EXECUTE_QUERY:
+				executeQuery(oc, sa.params);
+			break;
+		}
+	}
+	
+	
+	
 	private void executeQuery(ObjectConnection oc, Object object) 
 	{
 		try
@@ -81,15 +75,15 @@ public class SqlService
 			int cols = meta.getColumnCount();		
 			for(int i=0;i<cols;i++)
 				result.addColumn(meta.getColumnLabel(i+1));
-		
+			
 			//-- data
 			while(rs.next())
 			{
 				Vector line = new Vector();
-			
+				
 				for(int i=0;i<cols;i++)
 					line.add(rs.getString(i+1));
-			
+				
 				result.addLine(line);
 			}
 			
@@ -107,32 +101,38 @@ public class SqlService
 	private void getTableNames(ObjectConnection oc)
 	{
 		Vector data = new Vector();
-        
+		
 		try
 		{
+	  		Connection connection = layer.openConnection();
 			DatabaseMetaData dbmd = connection.getMetaData();
 			String[] types = {"TABLE", "VIEW"};
 			ResultSet rs = dbmd.getTables(null,null,null,types);
-        
+			
 			//field n°3 is the table name
 			while(rs.next())
 				data.addElement(rs.getString(3));
 			
+			rs.close();
+			connection.close();
 			oc.write(data);
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-   	}
+	}
 	
 	
 	private void getDriverInfo(ObjectConnection oc) 
 	{
 		try
 		{
+			Connection connection = layer.openConnection();
 			DatabaseMetaData dbmd = connection.getMetaData();
 			String info = dbmd.getDriverName() + " " + dbmd.getDriverVersion();
+			connection.close();
+			
 			oc.write(info);			
 		} catch(Exception e) {
 			e.printStackTrace();
