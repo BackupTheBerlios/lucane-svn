f80e8ead39541d71076050172375c0e944770a33
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-06-30 17:02:58 +0000

    forumadmin does not have a permanent connection anymore

--- trunk/applications/forumadmin/src/org/lucane/applications/forumadmin/ForumAdminService.java
+++ trunk/applications/forumadmin/src/org/lucane/applications/forumadmin/ForumAdminService.java
@@ -25,120 +25,117 @@ import java.util.*;
 
 
 public class ForumAdminService
-  extends Service
+extends Service
 {
-
-  private Connection conn = null;
-
-  public ForumAdminService()
-  {
-  }
-
-  public void process(ObjectConnection sc, Message message)
-  {
-	String line = (String)message.getData();
-
-    if(line != null)
-    {
-      StringTokenizer stk = new StringTokenizer(line);
-      String command = stk.nextToken();
-      String data = null;
-
-      try
-      {
-        data = stk.nextToken("\0").substring(1);
-      }
-      catch(Exception ex)
-      {
-        data = "";
-      }
-
-      /*
-       * known commands :
-       * LIST
-       * DELETE <forum>
-       * CREATE <forum>
-       */
-      if(command.equals("LIST"))
-        this.list(sc);
-      else if(command.equals("DELETE"))
-        this.delete(data, sc);
-      else if(command.equals("CREATE"))
-        this.create(data, sc);
-    }
-  }
-
-
-  public void init(Server parent)
-  {
-    try
-    {
-      conn = parent.getDBLayer().openConnection();
-    }
-    catch(Exception e)
-    {
-		Logging.getLogger().warning("ForumAdminService::error during initialization : " + e);
-    }
-  }
-
-  private void list(ObjectConnection sc)
-  {
-    try
-    {
-    	Vector v = new Vector();
-    	PreparedStatement select = conn.prepareStatement("SELECT name FROM forum");
-      ResultSet res = select.executeQuery();
-
-      while(res.next())
-        v.addElement(res.getString(1));
-
-      res.close();
-      select.close();
-      sc.write(v);
-    }
-    catch(Exception e)
-    {
-		Logging.getLogger().warning("Error in ForumService::list() : " + e);
-    }
-  }
-
-  private void delete(String forum, ObjectConnection sc)
-  {
-  	try
-    {
-  		PreparedStatement delete = conn.prepareStatement("DELETE FROM forum WHERE name= ?");
-  		delete.setString(1, forum);
-  		delete.execute();
-      sc.write("OK");
-      delete.close();
-    }
-    catch(Exception e)
-    {
-      try {
-      	sc.write("FAILED");
-      } catch(Exception e2) {}
-	  Logging.getLogger().warning("Error in ForumService::delete() : " + e);
-    }
-  }
-
-  private void create(String forum, ObjectConnection sc)
-  {
-
-    try
-    {
-    	PreparedStatement insert = conn.prepareStatement(
-    			"INSERT INTO forum VALUES(?)");
-    	insert.setString(1, forum);
-      insert.execute();
-      insert.close();
-      sc.write("OK");
-    }
-    catch(Exception e)
-    {
-      try {
-          sc.write("FAILED");
-      } catch(Exception e2) {}
-	  Logging.getLogger().warning("Error in ForumService::create() : " + e);
-    }
-  }
+	
+	private Connection conn = null;
+	
+	public ForumAdminService()
+	{
+	}
+	
+	public void process(ObjectConnection sc, Message message)
+	{
+		String line = (String)message.getData();
+		
+		if(line != null)
+		{
+			StringTokenizer stk = new StringTokenizer(line);
+			String command = stk.nextToken();
+			String data = null;
+			
+			try
+			{
+				data = stk.nextToken("\0").substring(1);
+			}
+			catch(Exception ex)
+			{
+				data = "";
+			}
+			
+			/*
+			 * known commands :
+			 * LIST
+			 * DELETE <forum>
+			 * CREATE <forum>
+			 */
+			if(command.equals("LIST"))
+				this.list(sc);
+			else if(command.equals("DELETE"))
+				this.delete(data, sc);
+			else if(command.equals("CREATE"))
+				this.create(data, sc);
+		}
+	}
+	
+	private void list(ObjectConnection sc)
+	{
+		try
+		{
+			conn = Server.getInstance().getDBLayer().openConnection();
+			
+			Vector v = new Vector();
+			PreparedStatement select = conn.prepareStatement("SELECT name FROM forum");
+			ResultSet res = select.executeQuery();
+			
+			while(res.next())
+				v.addElement(res.getString(1));
+			
+			res.close();
+			select.close();
+			conn.close();
+			
+			sc.write(v);
+		}
+		catch(Exception e)
+		{
+			Logging.getLogger().warning("Error in ForumService::list() : " + e);
+		}
+	}
+	
+	private void delete(String forum, ObjectConnection sc)
+	{
+		try
+		{
+			conn = Server.getInstance().getDBLayer().openConnection();
+			
+			PreparedStatement delete = conn.prepareStatement("DELETE FROM forum WHERE name= ?");
+			delete.setString(1, forum);
+			delete.execute();
+			sc.write("OK");
+			delete.close();
+			conn.close();
+		}
+		catch(Exception e)
+		{
+			try {
+				sc.write("FAILED");
+			} catch(Exception e2) {}
+			Logging.getLogger().warning("Error in ForumService::delete() : " + e);
+		}
+	}
+	
+	private void create(String forum, ObjectConnection sc)
+	{
+		
+		try
+		{
+			conn = Server.getInstance().getDBLayer().openConnection();
+			
+			PreparedStatement insert = conn.prepareStatement(
+			"INSERT INTO forum VALUES(?)");
+			insert.setString(1, forum);
+			insert.execute();
+			insert.close();
+			conn.close();
+			sc.write("OK");
+		}
+		catch(Exception e)
+		{
+			try {
+				sc.write("FAILED");
+			} catch(Exception e2) {}
+			Logging.getLogger().warning("Error in ForumService::create() : " + e);
+		}
+	}
 }
