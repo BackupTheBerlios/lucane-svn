b5d0170695b4b49d048a7aa0563e0eff02209928
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-07-02 22:35:44 +0000

    changed server.sh so that the script can be run from either : server/bin, server, or any other directory if $LUCANE_SERVER or $LUCANE_HOME is set

--- trunk/server/bin/server.sh
+++ trunk/server/bin/server.sh
@@ -1,4 +1,16 @@
-cd ..
+# go in the server directory 
+if [ -n "$LUCANE_SERVER" ]; then
+  cd $LUCANE_SERVER;
+elif [ -n "$LUCANE_HOME" ]; then
+  cd $LUCANE_HOME/server;
+elif [ -e "server.sh" ]; then
+  cd ..;
+elif [ -e "bin/server.sh" ]; then
+  cd .;
+else
+  echo "unable to find server, set $LUCANE_HOME or $LUCANE_SERVER !"
+  exit 1;
+fi
 
 # classpath generation
 for file in lib/*.jar
