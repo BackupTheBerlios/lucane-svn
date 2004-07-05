88f8695624e0c13800eaf577e7da32ad6f11a738
vfiack <vfiack@7c35f5e9-26d5-0310-982e-e5c3e75ee539>
2004-07-05 18:41:53 +0000

    .bat files are now similar to .sh ones

--- trunk/server/bin/server.bat
+++ trunk/server/bin/server.bat
@@ -1,8 +1,41 @@
-echo off
-cd ..
+@echo off
+
+rem go in the server directory
+if not "%LUCANE_SERVER%" == "" (
+  cd "%LUCANE_SERVER%"
+  goto fileFound
+)
+if not "%LUCANE_HOME%" == "" (
+  cd "%LUCANE_HOME%\server"
+  goto fileFound
+)
+if exist server.bat (
+  cd ..
+  goto fileFound
+)
+if exist bin\server.bat (
+  goto fileFound
+)
+if exist server\bin\server.bat (
+  cd server
+  goto fileFound
+)
+
+echo ooops
+goto end
+
+:fileFound
+
+rem classpath generation
 echo.set TMPCLASSPATH=%%1;%%TMPCLASSPATH%%>~tmp.bat
 set TMPCLASSPATH=
 for %%i in (lib\*.jar) do call ~tmp.bat "%CD%\%%i"
 del /F ~tmp.bat
+
+rem run programm
 java -classpath %TMPCLASSPATH% org.lucane.server.Server %1
+
+rem clean
 set TMPCLASSPATH=
+
+:end
